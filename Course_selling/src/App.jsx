ad107a8b0d12e73aeee7c1d127880937b358b707
nIkHiL <zero.zero619.ns@gmail.com>
2023-07-09 17:52:06 +0530

    added appbar

--- Course_selling/src/App.jsx
+++ Course_selling/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Signup from "./Signup";
+import Appbar from "./Appbar";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,13 +12,11 @@ function App() {
     <div
       style={{ width: "100vw", height: "100vh", backgroundColor: "#eeeeee" }}
     >
+      <Appbar></Appbar>
       <Signup></Signup>
       {/* <TodoApp nikhil={"name"}></TodoApp> */}
     </div>
   );
-}
-function TodoApp(props) {
-  return <div>{props.nikhil}</div>;
 }
 
 export default App;
