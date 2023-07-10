2dc6f55670f29b509eaac0da6ec8f88b204ad857
nIkHiL <zero.zero619.ns@gmail.com>
2023-07-10 08:53:52 +0530

    added express and node

---
+++ Course_selling/src/Appbar.jsx
@@ -0,0 +1,12 @@
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+function Appbar() {
+  return (
+    <div>
+      <Typography>Courses</Typography>
+      <Button></Button>
+    </div>
+  );
+}
+export default Appbar;
